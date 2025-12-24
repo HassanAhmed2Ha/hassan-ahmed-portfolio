86b44fe9939a5ce9b1b82ca136de6380afe164c7
Hassan2007 <hassanahmed07.e9@gmail.com>
2025-12-24 21:29:33 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // 👈 هذا السطر مهم جداً لتشغيل تصميمات Tailwind
 
 const rootElement = document.getElementById('root');
 
