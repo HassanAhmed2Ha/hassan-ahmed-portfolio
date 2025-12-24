7aca4479453874c1f757f4209ed59f006a5d9ee8
Hassan2007 <hassanahmed07.e9@gmail.com>
2025-12-24 22:42:48 +0200

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -12,7 +12,7 @@ export const contentEn: Content = {
   hero: {
     greeting: 'Hi there,',
     namePrefix: "I'm",
-    name: 'Hassan Ahmed',
+    name: 'Hassan Ahmed Hassan',
     role: ['Bioinformatics Researcher', 'Data Scientist', 'Erasmus+ Trainee', 'Digital Safety Advocate'],
     description: 'Bioinformatics and Data Science researcher focused on applying technology to improve public health, digital safety, and sustainable development in the MENA region.',
     btnAbout: 'About Me',
